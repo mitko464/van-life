b59da167c345069ba1597fc4924921b1281bc9e1
Dimitar Pushtiev <mitko464@gmail.com>
2023-12-19 11:04:27 +0200

    ADD: Van details page and van related sub-pages

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -63,8 +63,17 @@ export default function HostVanDetail() {
                         Photos
                     </NavLink>
                 </nav>
-                <Outlet />
+                <Outlet context={{ currentVan }} />
             </div>
         </section>
     )
 }
+
+/**
+ * Challenge: check out the docs linked in the slide, and see if you
+ * can implement the Outlet Context feature it talks about.
+ * 
+ * Part of this challenge will require you to (finally) build out those
+ * nested components. Again, if you don't need CSS practice, you can
+ * skip the styling part, and I'll handle that for you.
+ */
