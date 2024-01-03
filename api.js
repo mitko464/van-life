deb110bbb03bb756e145650c1800b2f38da7612d
Dimitar Pushtiev <mitko464@gmail.com>
2024-01-03 10:00:01 +0200

    ADD: Link to a real database

--- api.js
+++ api.js
@@ -1,29 +1,47 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs, getDoc, query, where } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfy4FY7u_mgWzygzx9MNLf2TBDOiKO9EQ",
+  authDomain: "vanlive-a7574.firebaseapp.com",
+  projectId: "vanlive-a7574",
+  storageBucket: "vanlive-a7574.appspot.com",
+  messagingSenderId: "841427246060",
+  appId: "1:841427246060:web:50112f8f969f8530a97ec9"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef);
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+
     }
-    const data = await res.json()
-    return data.vans
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
 
 export async function loginUser(creds) {
